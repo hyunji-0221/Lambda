eea29d424d143cb1d5247f37b6110d432277aeaa
hyunji-0221 <hyunji1443@gmail.com>
2024-03-07 12:09:11 +0900

    240307

---
+++ user/UserRepository.java
@@ -0,0 +1,24 @@
+package user;
+
+import java.sql.*;
+
+public class UserRepository {
+
+    public void addUsers() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/adadb";
+        String userName = "root";
+        String password = "rootroot";
+
+        Connection connection = DriverManager.getConnection(url, userName, password);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from board");
+
+        resultSet.next();
+        String name = resultSet.getString("name");
+        System.out.println(name);
+
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+}
